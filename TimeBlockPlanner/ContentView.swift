56aab81a43f9b73d9274664fd1fb2a7c836c9fa8
zvbchenko <zvbchenko@gmail.com>
2021-09-04 15:00:26 -0700

    Initial Commit

---
+++ TimeBlockPlanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimeBlockPlanner
+//
+//  Created by Anton Zubchenko on 2021-09-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeBlockPlanner/TimeBlockPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBlockPlannerApp.swift
+//  TimeBlockPlanner
+//
+//  Created by Anton Zubchenko on 2021-09-04.
+//
+
+import SwiftUI
+
+@main
+struct TimeBlockPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
