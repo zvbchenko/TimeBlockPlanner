d1f2931e17b7f0eecee539fe971362289a935183
zvbchenko <zvbchenko@gmail.com>
2021-09-08 23:17:52 -0700

    Added background placeholder, changed some color to the project subtask

--- TimeBlockPlanner/Subtask.swift
+++ TimeBlockPlanner/Subtask.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+
 struct Subtask: View {
-    @State var text: String = "Describe your Subtask"
+    @State var text: String = ""
+    var placeholder: String = "Describe your Subtask"
     var body: some View {
         ZStack{
-            TextEditor(text: $text)
+            
+            if text.isEmpty {
+              Text(placeholder)
+                .foregroundColor(Color.primary.opacity(0.25))
+                .padding(EdgeInsets(top: 7, leading: 4, bottom: 0, trailing: 0))
+                .padding(5)
+            }
+            TextEditor(text: $text).textFieldStyle(PlainTextFieldStyle()).font(Font.system(size: 10, design: .default))
             Text(text).opacity(0).padding(.all, 1)
         }.shadow(radius: 0)
         
         
+    }
+}
+
+extension NSTextView {
+    open override var frame: CGRect {
+        didSet {
+            backgroundColor = .clear //<<here clear
+            drawsBackground = true
+        }
+
     }
 }
 
