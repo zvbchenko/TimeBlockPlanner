d9ce3c85c3cccdf21c527f9f7479247bb40b4b1f
zvbchenko <zvbchenko@gmail.com>
2021-09-11 22:31:44 -0700

    Added tubtask addition field -> need to push it to the list of tasks next

--- TimeBlockPlanner/ProjectTemplate.swift
+++ TimeBlockPlanner/ProjectTemplate.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct EmptyProject: View {
     @State private var projectName: String = ""
+    @State private var subtask: String = ""
     @State private var dueDate = Date()
    
     var body: some View {
@@ -28,8 +29,36 @@ struct EmptyProject: View {
             ).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
             
             
-            Text("Describe a subtask")
+            
             Subtask().textFieldStyle(PlainTextFieldStyle()).font(Font.system(size: 10, design: .default))
+            
+            // Defining subtasks
+            ZStack {
+                Rectangle().fill(Color.gray)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 30)
+                    .cornerRadius(10).shadow(color: .gray, radius: 4).padding(20)
+                
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: nil, content:
+                {
+                    // "add" button
+                    Image(systemName: "plus")
+                        .resizable()
+                        .padding(6)
+                        .frame(width: 24, height: 24)
+                        .clipShape(Circle())
+                        .foregroundColor(.black).padding(25)
+                    
+                    VStack (alignment: .leading){
+                        TextField("Add task",text: $subtask).font(.headline)
+                            .lineLimit(1).foregroundColor(.black).textFieldStyle(PlainTextFieldStyle())
+
+                    }.padding(30)
+                    Spacer()
+            
+                }
+                )
+                
+            }
                 
             Text("Project Plan:")
             Text("List of subtasks")
